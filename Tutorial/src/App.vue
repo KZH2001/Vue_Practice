b356f6d5c06e8a0b3ced26b2d4a0fd7ec89ec7d1
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-01-18 23:07:11 +0630

    Update App.vue

--- Tutorial/src/App.vue
+++ Tutorial/src/App.vue
@@ -1,32 +1,48 @@
 
 
-<template>
-<h3>The total reusult is {{ add(2,3,4) }}</h3>
+  <template>
+  <h3>The total reusult is {{ add(2,3,4) }}</h3>
+  <h3>{{ number  }}</h3>
 
-<h3>{{ name }}</h3>
-<button  v-on:click="name = 'Batman'">Change name</button>
-</template>
+  <h3>{{ name }}</h3>
+  <button  v-bind:style="{color:color}" v-on:click="name = 'Batman'">Change name</button>
+  <button @click="increasNumber(1)">Increase 1</button>
+  <button @click="increasNumber(5)">Increase 5</button>
+  <button @click="decreasNumber(1)">Decrease 1</button>
+  <button @click="decreasNumber(5)">Decrease 5</button>
 
-<style scoped>
+  </template>
 
-</style>
+  <style scoped>
 
-<script >
-  export default{
+  </style>
 
-    
-    data(){
-      return{
-        name: 'Superman'
+  <script >
+    export default{
+
+      
+      data(){
+        return{
+          name: 'Superman',
+          color: 'red',
+          number: 0
+        }
+      },
+      methods: {
+      add(a,b,c){
+        return a+b+c
+      },
+
+      increasNumber(num){
+        this.number += num
+      },
+
+      decreasNumber(num){
+        this.number -= num
       }
-    },
-     methods: {
-    add(a,b,c){
-      return a+b+c
-    },
+
+    }
 
   }
-
-}
- 
-</script>
+  
+  </script>
