ef16def90585fa43a89ad5964ffb75a1c08adb53
Kyaw Zaw Hein <kyaw.zh@mit.com.mm>
2026-01-18 16:51:12 +0630

    update

---
+++ Tutorial/src/App.vue
@@ -0,0 +1,32 @@
+
+
+<template>
+<h3>The total reusult is {{ add(2,3,4) }}</h3>
+
+<h3>{{ name }}</h3>
+<button  v-on:click="name = 'Batman'">Change name</button>
+</template>
+
+<style scoped>
+
+</style>
+
+<script >
+  export default{
+
+    
+    data(){
+      return{
+        name: 'Superman'
+      }
+    },
+     methods: {
+    add(a,b,c){
+      return a+b+c
+    },
+
+  }
+
+}
+ 
+</script>
