2534d22d85f3340299331edaa8e909393deacabf
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-01-28 21:42:53 +0630

    Replacing methods

--- Tutorial/src/components/DemoOne.vue
+++ Tutorial/src/components/DemoOne.vue
@@ -1,40 +1,46 @@
 <template>
-    <div>
-        <h3>{{ firstName }} {{ lastName }}</h3>
-    </div>
+  <div>
+    <h3>{{ firstName }} {{ lastName }}</h3>
+    <button @click="increaseNumber">Count: {{ count }}</button>
+    <button @click="changeHero">Change Hero</button>
+  </div>
 </template>
 
 <script>
-        import  {reactive, toRefs}  from 'vue' 
-    export default {
+import { reactive, toRefs, ref } from 'vue'
 
-        setup(){
-            // let name = ref("Kyaw");
-            const state = reactive({
-                firstName: "Kyaw Zaw",
-                lastName: "Hein",
-            })
-            
-            setTimeout(() => {
-                state.firstName = "Thu Zar"
-                state.lastName = "Nwe"
-                
-            },2000)
+export default {
+  setup() {
+    const state = reactive({
+      firstName: "Kyaw Zaw",
+      lastName: "Hein"
+    })
 
-            return toRefs(state)
-            
-        },
-        data(){
-            return {
-                fname: "k",
-                lname: 'z'
-            }
-        }
+    setTimeout(() => {
+      state.firstName = "Thu Zar"
+      state.lastName = "Nwe"
+    }, 2000)
 
-        
+    const count = ref(0)
+
+    function increaseNumber() {
+      count.value++
     }
+
+    function changeHero() {
+      state.firstName = "Te"
+      state.lastName = "Cv"
+    }
+
+    return {
+      ...toRefs(state),
+      count,
+      increaseNumber,
+      changeHero
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 </style>
